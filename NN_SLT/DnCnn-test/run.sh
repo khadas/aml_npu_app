86676ee4dbef7640da224e023a4d7985a005e6c3
cancan.chang <cancan.chang@amlogic.com>
2019-03-06 18:36:06 +0800

    npu_app: release slt test dncnn-6.3.2.5 [3/3]
    PD#SWPL-4874
    
    Problem:
    RevB platform can not excuite slt-test
    
    Solution:
    release slt-test for revB
    
    Verify:
    W400
    
    Change-Id: If3de2abbc1bde229b1ec8e3ea725a4d89a9b2436
    Signed-off-by: cancan.chang@amlogic.com

--- NN_SLT/DnCnn-test/run.sh
+++ NN_SLT/DnCnn-test/run.sh
@@ -2,4 +2,5 @@
 path=$(cd `dirname $0`;pwd)
 cd $path
 export VSI_NN_LOG_LEVEL=0
-./dncnn DnCNN.export.data inp_1_out0_1_640_640_3_nchw.tensor 1000 10
+export VNN_LOOP_TIME=10
+./dncnn DnCNN.export.data inp_2.tensor
