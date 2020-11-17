66a3a9f4d91922758bd1c3af1cc1608ec5902e06
yan <yan-wyb@foxmail.com>
2020-11-17 11:07:32 +0800

    update yolov2 demo to DDK version 6.4.3CB
    Signed-off-by: yan <yan-wyb@foxmail.com>

--- DDK_6.3.3.4/detect_library/model_code/detect_yolo_v2/yolov2_process.c
+++ DDK_6.3.3.4/detect_library/model_code/detect_yolo_v2/yolov2_process.c
@@ -43,6 +43,8 @@ void yolov2_preprocess(input_image_t imageData, uint8_t *ptr)
 }
 
 /* Postprocess */
+//char *voc_names[] = {"aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"};
+
 static char *coco_names[] = {"person","bicycle","car","motorbike","aeroplane","bus","train","truck","boat","traffic light","fire hydrant","stop sign","parking meter","bench","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra","giraffe","backpack","umbrella","handbag","tie","suitcase","frisbee","skis","snowboard","sports ball","kite","baseball bat","baseball glove","skateboard","surfboard","tennis racket","bottle","wine glass","cup","fork","knife","spoon","bowl","banana","apple","sandwich","orange","broccoli","carrot","hot dog","pizza","donut","cake","chair","sofa","pottedplant","bed","diningtable","toilet","tvmonitor","laptop","mouse","remote","keyboard","cell phone","microwave","oven","toaster","sink","refrigerator","book","clock","vase","scissors","teddy bear","hair drier","toothbrush"};
 
 static float overlap(float x1, float w1, float x2, float w2)
